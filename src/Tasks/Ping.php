3221a3c93be710866c1ca2ce1a56f64ed08423ff
Emil Moe <emil@moegroup.dk>
2020-01-11 23:01:43 +0100

    Update

--- src/Tasks/Ping.php
+++ src/Tasks/Ping.php
@@ -12,42 +12,42 @@ class Ping
     /**
      * 
      */
-    public static function before(Schedule $schedule): Closure
+    public static function before(Schedule $schedule): string
     {
-        return function() use($schedule) {
-            $event = self::event($schedule);
+        $event = self::event($schedule);
 
-            if (is_null($event->output) || $event->output == $event->getDefaultOutput()) {
-                $event->sendOutputTo(storage_path('logs/schedule-'.sha1($event->mutexName()).'.log'));
-            }
+        if (is_null($event->output) || $event->output == $event->getDefaultOutput()) {
+            $event->sendOutputTo(storage_path('logs/schedule-'.sha1($event->mutexName()).'.log'));
+        }
 
-            Webhook::send('task', json_encode([
-                'data' => [
-                    'command' => self::command($event),
-                    'cron' => $event->expression,
-                    'description' => $event->description,
-                ],
-                'event' => 'before',
-            ]));
-        };
+        Webhook::send('task', json_encode([
+            'data' => [
+                'command' => self::command($event),
+                'cron' => $event->expression,
+                'description' => $event->description,
+            ],
+            'event' => 'before',
+        ]));
+
+        return '';
     }
 
     /**
      * 
      */
-    public static function after(Schedule $schedule): Closure
+    public static function after(Schedule $schedule): string
     {
-        return function() use($schedule) {
-            $event = self::event($schedule);
+        $event = self::event($schedule);
 
-            Webhook::send('task', json_encode([
-                'data' => [
-                    'command' => self::command($event),
-                    'output' => $event->exitCode === 0 ? '' : @file_get_contents($event->output),
-                ],
-                'event' => $event->exitCode === 0 ? 'success' : 'failure',
-            ]));
-        };
+        Webhook::send('task', json_encode([
+            'data' => [
+                'command' => self::command($event),
+                'output' => $event->exitCode === 0 ? '' : @file_get_contents($event->output),
+            ],
+            'event' => $event->exitCode === 0 ? 'success' : 'failure',
+        ]));
+
+        return '';
     }
 
     /**
