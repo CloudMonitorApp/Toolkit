c3396813ccdeba4a00c68a56bb8815e2e2b80cd9
Emil Moe <emil@moegroup.dk>
2020-01-11 14:26:27 +0100

    Don't fail on missing file

--- src/Tasks/Ping.php
+++ src/Tasks/Ping.php
@@ -17,11 +17,9 @@ class Ping
         $event = self::event($schedule);
 
         if (is_null($event->output) || $event->output == $event->getDefaultOutput()) {
-            $event->sendOutputTo(storage_path('logs/schedule-'.sha1($event->mutexName()).'.log'));
+            $event->sendOutputTo(storage_path('logs/schedule-'.sha1($event->mutexName()).'.log');
         }
-
-        echo $event->command;
-
+        
         Webhook::send('task', json_encode([
             'data' => [
                 'command' => self::command($event),
@@ -44,7 +42,7 @@ class Ping
         Webhook::send('task', json_encode([
             'data' => [
                 'command' => self::command($event),
-                'output' => $event->exitCode === 0 ? '' : file_get_contents($event->output),
+                'output' => $event->exitCode === 0 ? '' : @file_get_contents($event->output),
             ],
             'event' => $event->exitCode === 0 ? 'success' : 'failure',
         ]));
