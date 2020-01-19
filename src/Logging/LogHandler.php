d98d525662403ddbcae4e20cce7c39f23a9d09d6
Emil Moe <emil@moegroup.dk>
2020-01-19 15:17:43 +0100

    User agent

--- src/Logging/LogHandler.php
+++ src/Logging/LogHandler.php
@@ -53,7 +53,7 @@ class LogHandler extends AbstractProcessingHandler
     {
         return [
             'ip' => Request::ip(),
-            'user_agent' => getenv('HTTP_USER_AGENT'),
+            'user_agent' => $HTTP_USER_AGENT,
             'user_id' => auth()->check() ? auth()->id() : null,
             'user' => auth()->check() ? auth()->user()->toJson() : null,
             'session' => $_SESSION ?? null,
