65cee60145c168502be466223b8e31ff480e03ad
Emil Moe <emil@moegroup.dk>
2020-03-26 16:18:57 +0100

    Allow both vendor and dev area

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,5 +9,10 @@ Route::post('cloudmonitor', function(Request $request) {
 
 Route::get('/js/cloudmonitor.js', function() {
     header('Content-Type: application/javascript');
+
+    if(env('APP_DEV') !== true) {
+        return file_get_contents(base_path('vendor/emilmoe/cloudmonitor/src/resources/js/CloudMonitor.js'));
+    }
+
     return file_get_contents(base_path('/CloudMonitorToolkit/src/resources/js/CloudMonitor.js'));
 });
