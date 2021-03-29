b096b2ce465aa27f90550dbab48ee6ce0d9b00cf
Emil Moe <emil@moegroup.dk>
2021-03-29 19:00:22 +0200

    Version

--- src/Core/CloudMonitor.php
+++ src/Core/CloudMonitor.php
@@ -16,7 +16,7 @@ class CloudMonitor
      * 
      * @var string
      */
-    const VERSION = '1.1.16';
+    const VERSION = '1.1.17';
 
     /**
      * Maximum number of segments to avoid high memory and cpu consumption.
